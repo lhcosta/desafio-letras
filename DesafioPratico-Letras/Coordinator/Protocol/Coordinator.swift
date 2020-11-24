5158d0db55617892afdb4fba71726ee6674c1476
Lucas <lucashpc91@gmail.com>
2020-11-24 12:56:11 -0300

    feat: remove child when dismiss

--- DesafioPratico-Letras/Coordinator/Protocol/Coordinator.swift
+++ DesafioPratico-Letras/Coordinator/Protocol/Coordinator.swift
@@ -8,8 +8,24 @@
 import UIKit
 
 protocol Coordinator: AnyObject {
-    var currentViewController: UIViewController? { get }
+    var parentCoordinator: Coordinator? { get }
     var navigationController: UINavigationController { get }
     var childCoordinators: [Coordinator] { get set }
     func start()
+    func childDidFinish(_ child: Coordinator)
+}
+
+//MARK:- Default methods
+extension Coordinator {
+    
+    /// Removendo child ao retornar
+    /// - Parameter child: child para ser removido
+    func childDidFinish(_ child: Coordinator) {
+        for (index, coordinator) in childCoordinators.enumerated() {
+            if coordinator === child {
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
+    }
 }
